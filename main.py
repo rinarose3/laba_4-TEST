95d4a7788bf44521c9c7dbc6a138adfae5b035f0
rinarose3 <ryzhkova_ie@mail.ru>
2022-10-18 18:17:34 +0300

    Главный файл коммита

---
+++ main.py
@@ -0,0 +1,12 @@
+def ca_cows():
+    cow = int(input())
+    if 11 <= cow % 100 <= 14:
+        return f"на поле {cow} коров"
+    else:
+        n = cow % 10
+        if n == 1:
+            return f"на поле {cow} корова"
+        elif 2 <= n <= 4:
+            return f"на поле {cow} коровы"
+        else:
+            return f"на поле {cow} коров"
